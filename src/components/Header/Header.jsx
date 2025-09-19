e4e3979de73ffe489870d5fc9d3bc3dac276347e
arian <arian.maki@yahoo.com>
2025-09-19 03:06:10 +0200

    handleSearch added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ import "react-date-range/dist/styles.css"; // main style file (react-date-range)
 import "react-date-range/dist/theme/default.css"; // theme css file (react-date-range)
 import { DateRange } from "react-date-range";
 import { format } from "date-fns";
+import { createSearchParams, useNavigate } from "react-router-dom";
 
 function Header() {
   const [destination, setDestination] = useState("");
@@ -32,6 +33,19 @@ function Header() {
 
   const dateRef = useRef();
   useOutsideClick(dateRef, "dateDropdown", () => setIsDateOpen(false));
+
+  const navigate = useNavigate();
+  function handleSearch() {
+    const encodedParams = createSearchParams({
+      date: JSON.stringify(date),
+      options: JSON.stringify(options),
+      destination,
+    });
+    navigate({
+      pathname: "/hotels",
+      search: encodedParams.toString(),
+    });
+  }
 
   return (
     <div className="header">
@@ -88,7 +102,7 @@ function Header() {
           <span className="seperator"></span>
         </div>
         <div className="headerSearchItem">
-          <button className="headerSearchBtn">
+          <button className="headerSearchBtn" onClick={handleSearch}>
             <HiSearch className="headerIcon" />
           </button>
         </div>
