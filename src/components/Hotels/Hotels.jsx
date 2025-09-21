2e1f4b99d2f965f76a520d105aec0acb39f93cd0
arian <arian.maki@yahoo.com>
2025-09-21 15:28:16 +0200

    getting filters from params

---
+++ src/components/Hotels/Hotels.jsx
@@ -0,0 +1,19 @@
+import { useSearchParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import Loader from "../Loader/Loader";
+
+function Hotels() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const destination = searchParams.get("destination");
+  const room = JSON.parse(searchParams.get("options"))?.room;
+  const { isLoading, data } = useFetch(
+    "http://localhost:5008/hotels",
+    `host_location_like=${destination || ""}&name_like=${
+      destination || ""
+    }&accommodates_gte=${room || 1}`
+  );
+  if (isLoading) return <Loader />;
+  return <div>{data.length}</div>;
+}
+
+export default Hotels;
