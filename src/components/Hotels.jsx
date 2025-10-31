9624abce62a8b30be22460fd8936ead263e426d7
arian <arian.maki@yahoo.com>
2025-10-31 14:31:58 +0100

    showing the current hotel

--- src/components/Hotels.jsx
+++ src/components/Hotels.jsx
@@ -3,18 +3,20 @@ import Loader from "./Loader";
 import { useHotels } from "../context/HotelsProvider";
 
 function Hotels() {
-  const { isLoading, hotels } = useHotels();
+  const { isLoading, hotels, currentHotel } = useHotels();
   if (isLoading) return <Loader />;
   return (
     <div className="searchList">
       <h2>Found {hotels.length} hotels:</h2>
       {hotels.map((item) => {
+        const isCurrent = item.id === currentHotel?.id;
         return (
           <Link
             key={item.id}
             to={`/hotels/${item.id}?lat=${item.latitude}&lng=${item.longitude}`}
           >
-            <div className="searchItem">
+            <div className={`searchItem ${isCurrent ? "current-hotel" : ""}`}>
+              {isCurrent ? <span className="current-badge">Current</span> : null}
               <img src={item.xl_picture_url} alt={item.name} />
               <div className="searchItemDesc">
                 <p className="location">{item.smart_location}</p>
