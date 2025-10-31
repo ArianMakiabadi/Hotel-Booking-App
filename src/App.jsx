e1c0287be9c73dbe0779d17f050900b0460a8e10
arian <arian.maki@yahoo.com>
2025-10-31 10:38:57 +0100

    single hotel added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Route, Routes } from "react-router-dom";
 import AppLayout from "./components/AppLayout";
 import Hotels from "./components/Hotels";
 import HotelsProvider from "./context/HotelsProvider";
+import SingleHotel from "./components/SingleHotel";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
         <Route path="/" element={<LocationList />} />
         <Route path="/hotels" element={<AppLayout />}>
           <Route index element={<Hotels />} />
-          <Route path=":id" element={<div>single hotel</div>} />
+          <Route path=":id" element={<SingleHotel />} />
         </Route>
       </Routes>
     </HotelsProvider>
